b67aea5ce8de1bdac48cb6499372df4a35a58798
neverov.iv@gmail.com <neverov.iv@gmail.com>
2024-04-03 22:21:57 +0300

    My first commit

--- homework/test_shop.py
+++ homework/test_shop.py
@@ -4,11 +4,17 @@
 import pytest
 
 from homework.models import Product
+from homework.models import Cart
 
 
 @pytest.fixture
 def product():
     return Product("book", 100, "This is a book", 1000)
+
+
+@pytest.fixture
+def cart():
+    return Cart()
 
 
 class TestProducts:
@@ -19,16 +25,26 @@ class TestProducts:
 
     def test_product_check_quantity(self, product):
         # TODO напишите проверки на метод check_quantity
-        pass
+        assert product.check_quantity(1000)
+        assert product.check_quantity(999)
+        assert product.check_quantity(0)
+        assert product.check_quantity(1001) == False
+        assert product.check_quantity(-1) == ValueError
 
     def test_product_buy(self, product):
         # TODO напишите проверки на метод buy
-        pass
+        product.buy(0)
+        assert product.check_quantity(1000)
+        product.buy(500)
+        assert product.check_quantity(500)
+        product.buy(500)
+        assert product.check_quantity(0)
 
     def test_product_buy_more_than_available(self, product):
         # TODO напишите проверки на метод buy,
         #  которые ожидают ошибку ValueError при попытке купить больше, чем есть в наличии
-        pass
+        with pytest.raises(ValueError):
+            product.buy(1001)
 
 
 class TestCart:
@@ -38,3 +54,53 @@ class TestCart:
         На некоторые методы у вас может быть несколько тестов.
         Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
     """
+
+    def test_add_product(self, cart, product):
+        cart.add_product(product, 5)
+        assert cart.products[product] == 5
+
+    def test_default_value_in_cart(self, cart, product):
+        cart.add_product(product)
+        assert cart.products[product] == 1
+
+    def test_remove_product(self, cart, product):
+        cart.add_product(product, 5)
+        cart.remove_product(product, 4)
+        assert cart.products[product] == 1
+
+    def test_remove_more_than_in_cart_products(self, cart, product):
+        cart.add_product(product, 5)
+        cart.remove_product(product, 6)
+        assert product not in cart.products
+
+    def test_amount_for_remove_is_none(self, cart, product):
+        cart.add_product(product, 10)
+        cart.remove_product(product, None)
+        assert product not in cart.products
+
+    def test_clear_cart(self, cart, product):
+        cart.add_product(product, 10)
+        cart.clear()
+        assert product not in cart.products
+
+    def test_get_total_price(self, cart, product):
+        cart.add_product(product, 15)
+        assert cart.get_total_price() == 1500
+
+    def test_get_total_price_when_ziro(self, cart, product):
+        cart.add_product(product, 0)
+        assert cart.get_total_price() == 0
+
+    def test_get_total_price_default(self, cart, product):
+        cart.add_product(product)
+        assert cart.get_total_price() == 100
+
+    def test_buy(self, cart, product):
+        cart.add_product(product, 100)
+        cart.buy()
+        assert product.quantity == 900
+
+    def test_buy_extra_amount(self, cart, product):
+        cart.add_product(product, 1001)
+        with pytest.raises(ValueError):
+            cart.buy()
